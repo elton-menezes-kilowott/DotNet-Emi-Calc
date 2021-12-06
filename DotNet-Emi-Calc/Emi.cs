4c4331bcef11b0b98680aa80b82c9011f0edf43c
elton-menezes-kilowott <92842347+elton-menezes-kilowott@users.noreply.github.com>
2021-12-06 13:43:30 +0530

    Add project files.

---
+++ DotNet-Emi-Calc/Emi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Emi_Calc
+{
+    public class Emi
+    {
+        public int Amount { get; set; }
+        public double ROI { get; set; }
+        public int Time { get; set; }
+
+        public double Calc_Emi()
+        {
+            double r = ROI / (12 * 100);
+            double upperLogic = Math.Pow(1 + r, Time * 12);
+            double Emi = Amount * r * (upperLogic / (upperLogic - 1));
+            return Emi;
+        }
+        
+    }
+}
---
+++ DotNet-Emi-Calc/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;   
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_Emi_Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter loan amount");
+            int loanAmount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter rate of interest");
+            double ROI = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter No of Years");
+            int time = Convert.ToInt32(Console.ReadLine());
+           
+            var emi = new Emi();
+            emi.Amount = loanAmount;
+            emi.ROI = ROI;
+            emi.Time = time;
+
+            double EMI = emi.Calc_Emi();
+            Console.WriteLine("Emi is : " + EMI);
+        }
+    }
+}
+
+    
